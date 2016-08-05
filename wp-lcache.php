45ba9a7090f5a6995d99cb83da4ee7f56ff9a0b4
Daniel Bachhuber <daniel@handbuilt.co>
2016-08-05 12:04:39 -0700

    Initialize the database schema when plugin is activated

--- wp-lcache.php
+++ wp-lcache.php
@@ -22,3 +22,25 @@
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+
+/**
+ * Create the requisite tables if they don't yet exist
+ */
+function wp_lcache_initialize_database_schema() {
+	global $wpdb;
+
+	$wpdb->query( "CREATE TABLE IF NOT EXISTS `lcache_events` (
+		`event_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+		`pool` varchar(255) NOT NULL DEFAULT '' COMMENT 'PHP process pool that wrote the change.',
+		`key` varchar(255) DEFAULT '' COMMENT 'Cache key within bin.',
+		`value` blob COMMENT 'A collection of data to cache.',
+		`expiration` int(11) DEFAULT NULL COMMENT 'A Unix timestamp indicating when the cache entry should expire, or NULL for never.',
+		`created` int(11) DEFAULT '0' COMMENT 'A Unix timestamp indicating when the cache entry was created.',
+		PRIMARY KEY (`event_id`),
+		UNIQUE KEY `event_id` (`event_id`),
+		KEY `expiration` (`expiration`),
+		KEY `lookup_miss` (`key`,`event_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;" );
+}
+
+register_activation_hook( __FILE__, 'wp_lcache_initialize_database_schema' );
