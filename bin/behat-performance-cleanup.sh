b6cfd26fb5e80728871980aebc5b6348b5581ada
Steve Persch <steve.persch@pantheon.io>
2016-08-24 12:47:09 -0500

    Cleanup fix

--- bin/behat-performance-cleanup.sh
+++ bin/behat-performance-cleanup.sh
@@ -11,6 +11,15 @@ set -ex
 
 PREPARE_DIR="/tmp/$TERMINUS_ENV-$TERMINUS_SITE"
 
+# The purpose of this section is to ensure that two CircleCI build are not
+# trying to run tests on the same multidev environment at the same time.
+IN_PROGRESS_INDICATOR_FILE="$PREPARE_DIR/performance_test_in_progress.txt"
+echo $IN_PROGRESS_INDICATOR_FILE
+if [ -f $IN_PROGRESS_INDICATOR_FILE ];
+  then echo "Another process or build is already running tests on this environment. Exiting.";
+  exit 0
+fi
+
 # Clean up by restoring this branch to match master
 cd $PREPARE_DIR
 git checkout master
