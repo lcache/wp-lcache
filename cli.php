e6b249920dacdec79c22af8bfd2595a9b706760e
Frankie Jarrett <fjarrett@gmail.com>
2017-04-28 00:05:38 -0500

    Always run table init on the `enable` CLI command

--- cli.php
+++ cli.php
@@ -6,6 +6,7 @@ class WP_LCache_CLI {
 	 * Enable WP LCache by creating a stub for object-cache.php
 	 */
 	public function enable() {
+		wp_lcache_initialize_database_schema();
 		if ( defined( 'WP_LCACHE_OBJECT_CACHE' ) && WP_LCACHE_OBJECT_CACHE ) {
 			WP_CLI::success( 'WP LCache is already enabled.' );
 			return;
